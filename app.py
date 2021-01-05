c10c8380d4c0e346c659ff493be37ad1f17ebc26
mtiemi <marianetiemi@gmail.com>
2021-01-05 01:53:04 -0300

    Major fixes and improvements

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, flash, request, redirect, url_for, render_template, send_file
-from werkzeug.utils import secure_filename
-import sqlite3
 from werkzeug.exceptions import abort
-import process
+import datetime
+import sqlite3
 import requests
 import json
 import logging
@@ -21,26 +20,9 @@ logger = logging.getLogger('werkzeug') # grabs underlying WSGI logger
 handler = logging.FileHandler('logging.log') # creates handler for the log file
 logger.addHandler(handler) # adds handler to the werkzeug WSGI logger
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def get_file(file_id):
-    conn = get_db_connection()
-    file = conn.execute('SELECT * FROM files WHERE id = ?',
-                        (file_id,)).fetchone()
-    conn.close()
-    if file is None:
-        abort(404)
-    return file    
-
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    files = conn.execute('SELECT * FROM files').fetchall()
-    conn.close()
-    return render_template('index.html', files=files)
+    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def upload_file_convert():
@@ -59,7 +41,7 @@ def upload_file_convert():
     if google_response['success']:
         if uploaded_file.filename != '' and request.form['btn'] == "convertMIDI":
             uploaded_file.filename = uploaded_file.filename[:-4]
-            file_processed = audio2midi.run(file_upload, output_file_name)
+            file_processed = audio2midi.run(uploaded_file,  uploaded_file.filename + '.mid')
             
             connection = sqlite3.connect('database.db')
             cur = connection.cursor()
@@ -68,8 +50,9 @@ def upload_file_convert():
                         (uploaded_file.filename,)) 
             connection.commit()
             connection.close()
-            logger.info("Converting wav file:" + uploaded_file.filename)
+            logger.info( '[{date:%d/%B/%Y %H:%M:%S}]'.format(date=datetime.datetime.now()) + " -  Converting wav file:" + uploaded_file.filename)
             return send_file(file_processed, attachment_filename= uploaded_file.filename + '.mid',as_attachment=True)  
     else:
-        logger.info("Recaptcha failed.")
+        logger.info( '[{date:%d/%B/%Y %H:%M:%S}]'.format(date=datetime.datetime.now()) + " - Recaptcha failed.")
+
         return render_template('index.html')
